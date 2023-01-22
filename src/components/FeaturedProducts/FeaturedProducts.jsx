9d059fc533fb3d08e84102e19c38fc5f65a6020e
Alferavil88 <alferavil88@gmail.com>
2023-01-21 22:12:35 -0500

    feat : Add featured products section

--- src/components/FeaturedProducts/FeaturedProducts.jsx
+++ src/components/FeaturedProducts/FeaturedProducts.jsx
@@ -1,7 +1,26 @@
 import "./FeaturedProducts.scss";
+import { ProductCard } from "../index";
+import { featured } from "../../utils/constants";
+import { Link } from "react-router-dom";
 
 const FeaturedProducts = () => {
-  return <div>FeaturedProducts</div>;
+  return (
+    <section className="featured-products">
+      <div className="title">
+        <h2>featured products</h2>
+        <div className="underline"></div>
+      </div>
+      <div className="featured section-center">
+        {featured.map((product) => (
+          <ProductCard key={product.id} {...product} />
+        ))}
+      </div>
+
+      <Link to="/products" className="btn featured__btn">
+        All Products
+      </Link>
+    </section>
+  );
 };
 
 export default FeaturedProducts;
--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,7 +1,21 @@
-import "./ProductsCard.scss";
+import "./ProductCard.scss";
+import { Link } from "react-router-dom";
 
-const ProductCard = () => {
-  return <div>ProductCard</div>;
+const ProductCard = ({ image, title, price, id }) => {
+  return (
+    <div className="product">
+      <div className="container">
+        <img src={image} alt={title} />
+        <Link to={`/products/${id}`}>
+          <span>More details</span>
+        </Link>
+      </div>
+      <div className="info">
+        <h5>{title.substring(0, 15)}...</h5>
+        <p>${price}</p>
+      </div>
+    </div>
+  );
 };
 
 export default ProductCard;
