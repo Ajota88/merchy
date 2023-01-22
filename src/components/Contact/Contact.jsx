f5bb09fce720d18621b824b17f97f7c96cd9fe23
Alferavil88 <alferavil88@gmail.com>
2023-01-21 22:22:20 -0500

    feat: Add contact section

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,7 +1,26 @@
 import "./Contact.scss";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <section className="contact | section-center">
+      <h3>Join our newsletter and get 20% off</h3>
+      <div className="content">
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+          Voluptatibus, pariatur facere! Consequuntur maiores ullam, eligendi
+          omnis officia quas quia vero.
+        </p>
+        <form className="contact-form">
+          <input
+            type="email"
+            className="form-input"
+            placeholder="enter email"
+          />
+          <button className="submit-btn">subscribe</button>
+        </form>
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
