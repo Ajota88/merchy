96f6aa4de1541e2b68cb24a694bdd668b813baf4
Alferavil88 <alferavil88@gmail.com>
2023-01-21 23:21:33 -0500

    feat: Add footer section

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,65 @@
+import logo from "../../assets/logo-white.svg";
+import { navLinks } from "../../utils/constants";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faTwitter,
+  faInstagram,
+  faFacebook,
+  faGoogle,
+} from "@fortawesome/free-brands-svg-icons";
+import { Link } from "react-router-dom";
 import "./Footer.scss";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="footer">
+      <div className="container">
+        <div className="logo-social">
+          <a herf="#">
+            <img src={logo} alt="logo" />
+          </a>
+        </div>
+
+        <nav className="footer-nav">
+          <ul role="list" aria-label="footer">
+            {navLinks.map((link) => (
+              <li key={link.id}>
+                <Link to={link.url}>{link.text}</Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+
+        <ul className="social-links" role="list" aria-label="social links">
+          <li>
+            <a href="#" aria-label="facebook">
+              <FontAwesomeIcon icon={faFacebook} />
+            </a>
+          </li>
+          <li>
+            <a href="#" aria-label="gamil">
+              <FontAwesomeIcon icon={faGoogle} />
+            </a>
+          </li>
+          <li>
+            <a href="#" aria-label="twitter">
+              <FontAwesomeIcon icon={faTwitter} />
+            </a>
+          </li>
+          <li>
+            <a href="#" aria-label="instagram">
+              <FontAwesomeIcon icon={faInstagram} />
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <div className="copyright">
+          <p> &copy; {new Date().getFullYear()}. All Rights Reserved</p>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
